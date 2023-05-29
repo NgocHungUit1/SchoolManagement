8892d54cf510e3a16bcb2a6ce41903cf202b91f5
cppsw-dev <dev@cppsw.com>
2023-05-29 10:01:28 +0700

    add average_score student and fix bug

--- app/Services/ExamService.php
+++ app/Services/ExamService.php
@@ -41,24 +41,12 @@ class ExamService
     /**
      * Get Exam Schedule .
      *
-     * @param int $examId  The ID of the exam
-     * @param int $classId The ID of the class
+     * @param int $examId     The ID of the exam
+     * @param int $classId    The ID of the class
+     * @param int $semesterId The ID of the semester
      *
      * @return \Illuminate\Http\JsonResponse
      */
-    public function getAcademicData($id, $semester_id)
-    {
-        $data['getClass'] = ClassModel::find($id);
-        $data['getSubject'] = ClassSubject::MySubject($id);
-        $data['getScore'] = ExamScore::getAcademicRecords($id, $semester_id);
-        $data['getExamSemester'] = Semester::whereIn('id', [1, 2])->get();
-        $data['getRecord'] = ClassModel::getClassAcademic();
-        $data['getStudent'] = User::getStudentClassExam($id);
-
-        return $data;
-    }
-
-
     public function getExamSchedule($examId, $classId, $semesterId)
     {
 
@@ -107,6 +95,27 @@ class ExamService
     }
 
     /**
+     * Get Academic score  .
+     *
+     * @param int $id          The ID of the class
+     * @param int $semester_id The ID of the semester
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function getAcademicData($id, $semester_id)
+    {
+        $data['getClass'] = ClassModel::find($id);
+        $data['getSubject'] = ClassSubject::MySubject($id);
+        $data['getScore'] = ExamScore::getAcademicRecords($id, $semester_id);
+        $data['getExamSemester'] = Semester::whereIn('id', [1, 2])->get();
+        $data['getRecord'] = ClassModel::getClassAcademic();
+        $data['getStudent'] = User::getStudentClassExam($id);
+
+        return $data;
+    }
+
+
+    /**
      * Exam Schedule Insert .
      *
      * @param Request $request Request object
@@ -116,11 +125,14 @@ class ExamService
     public function examScheduleInsert(Request $request)
     {
 
-        ExamSchedule::deleteRecord($request->exam_id, $request->class_id, $request->semester_id);
+        ExamSchedule::deleteRecord(
+            $request->exam_id,
+            $request->class_id,
+            $request->semester_id
+        );
 
         foreach ($request->schedule as $schedule) {
-            if (
-                !empty($schedule['subject_id'])
+            if (!empty($schedule['subject_id'])
                 && !empty($schedule['exam_date'])
                 && !empty($schedule['start_time'])
                 && !empty($schedule['end_time'])
@@ -150,7 +162,7 @@ class ExamService
                         'full_mark' => $schedule['full_mark'],
                         'passing_mark' => $schedule['passing_mark'],
                         'created_by' => Auth::user()->id,
-                        'semester_id' => $request->semester_id // Lưu exam_schedule theo học kì
+                        'semester_id' => $request->semester_id
                     ]
                 );
                 $save->save();
@@ -233,180 +245,93 @@ class ExamService
 
         return $overlapping;
     }
+
     /**
-     * Insert Score.
+     * Insert exam scores for a given class, subject, and semester.
      *
-     * @param Request $request Request object
+     * @param Request $request The HTTP request object.
      *
-     * @return \Illuminate\View\View
+     * @return RedirectResponse
      */
     public function insertScore(Request $request)
     {
-
-        ExamScore::where('class_id', '=', $request->class_id)
-            ->where('subject_id', '=', $request->subject_id)
-            ->where('semester_id', '=', $request->semester_id)
+        ExamScore::where('class_id', $request->class_id)
+            ->where('subject_id', $request->subject_id)
+            ->where('semester_id', $request->semester_id)
             ->delete();
-        $examScores = $request->input('exam_score');
-        foreach ($examScores as $studentId => $scores) {
+
+        foreach ($request->exam_score as $studentId => $scores) {
             $total = 0;
             $total_weight = 0;
-            foreach ($scores as $scoreData) {
 
-                if (
-                    !empty($studentId)
-                    && !empty($scoreData['exam_id'])
-                    && !empty($scoreData['score'])
-                ) {
+            foreach ($scores as $scoreData) {
+                if ($this->validateScoreData($studentId, $scoreData)) {
                     $exam = Exam::find($scoreData['exam_id']);
                     $subtotal = $scoreData['score'] * $exam->description;
                     $total += $subtotal;
                     $total_weight += $exam->description;
-                    ExamScore::create(
-                        [
-                            'exam_id' => $scoreData['exam_id'],
-                            'class_id' => $request->input('class_id'),
-                            'semester_id' => $request->input('semester_id'),
-                            'subject_id' => $request->input('subject_id'),
-                            'student_id' => $studentId,
-                            'score' => $scoreData['score'],
-                            'created_by' => Auth::user()->id,
-                        ]
-                    );
+                    $this->createExamScoreRecord($request, $studentId, $scoreData);
                 }
             }
+
             $this->calculateStudentAverage($request, $studentId, $total, $total_weight);
         }
 
-        $data['getStudent'] = User::getStudentClassExam($request->input('class_id'));
-        $data['getSubject'] = ClassSubject::MySubject($request->input('class_id'));
-        $data['getScore'] = ExamScore::getAcademicRecords(
-            $request->input('class_id'),
-            $request->input('semester_id')
+        $this->updateStudentScores($request);
+
+        return redirect()->back()->with('success', 'Exam scores have been saved.');
+    }
+
+    /**
+     * Validate score data for a given student and exam.
+     *
+     * @param mixed $studentId The student ID.
+     * @param array $scoreData The exam score data.
+     *
+     * @return bool
+     */
+    public function validateScoreData($studentId, $scoreData)
+    {
+        return !empty($studentId)
+            && !empty($scoreData['exam_id'])
+            && !empty($scoreData['score']);
+    }
+
+    /**
+     * Create an exam score record for a given student and score.
+     *
+     * @param Request $request   The HTTP request object.
+     * @param mixed   $studentId The student ID.
+     * @param array   $scoreData The exam score data.
+     *
+     * @return void
+     */
+    public function createExamScoreRecord($request, $studentId, $scoreData)
+    {
+        ExamScore::create(
+            [
+                'exam_id' => $scoreData['exam_id'],
+                'class_id' => $request->class_id,
+                'semester_id' => $request->semester_id,
+                'subject_id' => $request->subject_id,
+                'student_id' => $studentId,
+                'score' => $scoreData['score'],
+                'created_by' => Auth::user()->id,
+            ]
         );
-
-        foreach ($data['getStudent'] as $student) {
-            $scored_subjects = []; // Khởi tạo một mảng tạm thời để lưu trữ các subject_id đã được chấm điểm của học sinh
-            $total_score = 0;
-            $total_subjects_scored = 0;
-
-            $total_average = 0;
-            foreach ($data['getSubject'] as $subject) {
-                $scores = $data['getScore']->where('subject_id', $subject->subject_id)
-                    ->where('student_id', $student->id)
-                    ->first()->score ?? '';
-                if (!empty($scores)) {
-                    $total_score += $scores;
-                    $total_subjects_scored++;
-
-                    // Lưu trữ subject_id của môn đã được chấm điểm vào mảng tạm thời
-                    $scored_subjects[] = $subject->subject_id;
-                }
-            }
-            // Tính giá trị $all_subjects dựa trên số lượng phần tử trong mảng getSubject
-            $all_subjects = count($data['getSubject']);
-
-            // Kiểm tra xem số môn đã được chấm điểm của học sinh có bằng tổng số môn hay không
-            if ($total_subjects_scored == $all_subjects) {
-                $average = number_format($total_score / $all_subjects, 2);
-                StudentScoreSemester::updateOrCreate(
-                    [
-                        'student_id' => $student->id,
-                        'semester_id' => $request->input('semester_id'),
-                    ],
-                    [
-                        'avage_score' => $average,
-                        'semester_id' => $request->input('semester_id'),
-                        'student_id' => $student->id
-                    ]
-                );
-            }
-            $semester_1_average_score = StudentScoreSemester::where('student_id', $student->id)
-                ->where('semester_id', 1)
-                ->first()->avage_score ?? null;
-            // Lấy điểm trung bình kỳ học 1 của học sinh
-
-            $semester_2_average_score = StudentScoreSemester::where('student_id', $student->id)
-                ->where('semester_id', 2)
-                ->first()->avage_score ?? null;
-            // Lấy điểm trung bình kỳ học 2 của học sinh
-
-            $total_average = $semester_1_average_score + $semester_2_average_score * 2;
-
-            // Tính điểm trung bình cả năm của học sinh
-            if ($total_average > 0 &&  $semester_1_average_score > 0 && $semester_2_average_score > 0) {
-                $yearly_average = number_format($total_average / 3, 2);
-            } else {
-                $yearly_average = null;
-            }
-
-            if (!isset($yearly_average)) {
-                $rank = null;
-            } else if ($yearly_average < 5) {
-                $rank = 'D';
-            } else if ($yearly_average >= 5 && $yearly_average < 6.5) {
-                $rank = 'C';
-            } else if ($yearly_average >= 6.5 && $yearly_average < 8) {
-                $rank = 'B';
-            } else {
-                $rank = 'A';
-            }
-            StudentScoreSemester::updateOrCreate(
-                ['student_id' => $student->id, 'semester_id' => 3],
-                [
-                    'avage_score' => $yearly_average,
-                    'rank' => $rank
-                ],
-            );
-        }
-
-
-        return redirect()->back()
-            ->with('success', 'Exam scores have been saved.');
     }
 
-    public function getAverages($students)
-    {
-        $averages = [];
-
-        foreach ($students as $student) {
-            $semester1Average = $this->getSemesterAverage($student->id, 1);
-            $semester2Average = $this->getSemesterAverage($student->id, 2);
-            $yearlyAverage = $this->getYearlyAverage($student->id);
-            $rank = $this->getRank($student->id);
-
-            $averages[$student->id] = [
-                'semester_1_average' => $semester1Average,
-                'semester_2_average' => $semester2Average,
-                'yearly_average' => $yearlyAverage,
-                'rank' => $rank,
-            ];
-        }
-
-        return $averages;
-    }
-
-    private function getSemesterAverage($studentId, $semesterId)
-    {
-        return StudentScoreSemester::where('student_id', $studentId)
-            ->where('semester_id', $semesterId)
-            ->value('avage_score');
-    }
-
-    private function getYearlyAverage($studentId)
-    {
-        return StudentScoreSemester::where('student_id', $studentId)
-            ->where('semester_id', 3)
-            ->value('avage_score');
-    }
-
-    private function getRank($studentId)
-    {
-        return StudentScoreSemester::where('student_id', $studentId)
-            ->value('rank');
-    }
-
-    private function calculateStudentAverage($request, $studentId, $total, $total_weight)
+    /**
+     * Calculate the average score for a given student.
+     *
+     * @param Request $request      The HTTP request object.
+     * @param mixed   $studentId    The student ID.
+     * @param float   $total        The total score for the student.
+     * @param float   $total_weight The total weight of all exams.
+     *
+     * @return void
+     */
+    public function calculateStudentAverage($request, $studentId, $total, $total_weight)
     {
         if ($total_weight > 0) {
             $average = $total / $total_weight;
@@ -428,11 +353,200 @@ class ExamService
         }
     }
 
+    /**
+     * Calculate the update average score for a given student.
+     *
+     * @param Request $request The HTTP request object.
+     *
+     * @return void
+     */
+    public function updateStudentScores($request)
+    {
+        $students = User::getStudentClassExam($request->class_id);
+        $subjects = ClassSubject::MySubject($request->class_id);
+        $scores = ExamScore::getAcademicRecords($request->class_id, $request->semester_id);
+
+        foreach ($students as $student) {
+            $scoredSubjects = $scores
+                ->where('student_id', $student->id)
+                ->pluck('subject_id')
+                ->toArray();
+
+            $totalScore = $scores
+                ->whereIn('subject_id', $scoredSubjects)
+                ->where('student_id', $student->id)
+                ->sum('score');
+
+            if (count($scoredSubjects) === count($subjects)) {
+                $this->calculateAndSaveSemesterAverage($student->id, $request->semester_id, $totalScore, $subjects);
+            }
+
+            if ($this->hasSufficientDataForYearlyAverage($student)) {
+                $this->calculateAndSaveYearlyAverage($student);
+            }
+        }
+    }
+
+    /**
+     * Calculate the average score for a given student.
+     *
+     * @param mixed $studentId  The student ID.
+     * @param int   $semesterId The ID of the semester
+     * @param float $totalScore The total score for the student.
+     * @param $subjects   The total weight of all exams.
+     *
+     * @return void
+     */
+    public function calculateAndSaveSemesterAverage($studentId, $semesterId, $totalScore, $subjects)
+    {
+        $average = number_format($totalScore / count($subjects), 2);
+
+        StudentScoreSemester::updateOrCreate(
+            ['student_id' => $studentId, 'semester_id' => $semesterId],
+            ['avage_score' => $average]
+        );
+    }
+
+    /**
+     * Calculate the average score for a given student.
+     *
+     * @param $student The student.
+     *
+     * @return void
+     */
+    public function hasSufficientDataForYearlyAverage($student)
+    {
+        $semester1Average = StudentScoreSemester::where('student_id', $student->id)
+            ->where('semester_id', 1)
+            ->value('avage_score');
+
+        $semester2Average = StudentScoreSemester::where('student_id', $student->id)
+            ->where('semester_id', 2)
+            ->value('avage_score');
+
+        return $semester1Average !== null && $semester2Average !== null;
+    }
+
+    /**
+     * Calculate the average score year for a given student.
+     *
+     * @param $student The student.
+     *
+     * @return void
+     */
+    public function calculateAndSaveYearlyAverage($student)
+    {
+        $semester_1_average_score = StudentScoreSemester::where('student_id', $student->id)
+            ->where('semester_id', 1)->value('avage_score');
+
+        $semester_2_average_score = StudentScoreSemester::where('student_id', $student->id)
+            ->where('semester_id', 2)
+            ->value('avage_score');
+
+        $total_average = $semester_1_average_score + ($semester_2_average_score * 2);
+
+        if ($total_average > 0 &&  $semester_1_average_score > 0 && $semester_2_average_score > 0) {
+            $yearly_average = number_format($total_average / 3, 2);
+        } else {
+            $yearly_average = null;
+        }
+
+        if (!isset($yearly_average)) {
+            $rank = null;
+        } else if ($yearly_average < 5) {
+            $rank = 'D';
+        } else if ($yearly_average >= 5 && $yearly_average < 6.5) {
+            $rank = 'C';
+        } else if ($yearly_average >= 6.5 && $yearly_average < 8) {
+            $rank = 'B';
+        } else {
+            $rank = 'A';
+        }
+        StudentScoreSemester::updateOrCreate(
+            ['student_id' => $student->id, 'semester_id' => 3],
+            [
+                'avage_score' => $yearly_average,
+                'rank' => $rank
+            ],
+        );
+    }
+
+    /**
+     * Get the average score  for a given student semester.
+     *
+     * @param $students The student.
+     *
+     * @return void
+     */
+    public function getAverages($students)
+    {
+        $averages = [];
+
+        foreach ($students as $student) {
+            $semester1Average = $this->getSemesterAverage($student->id, 1);
+            $semester2Average = $this->getSemesterAverage($student->id, 2);
+            $yearlyAverage = $this->getYearlyAverage($student->id);
+            $rank = $this->getRank($student->id);
+
+            $averages[$student->id] = [
+                'semester_1_average' => $semester1Average,
+                'semester_2_average' => $semester2Average,
+                'yearly_average' => $yearlyAverage,
+                'rank' => $rank,
+            ];
+        }
+
+        return $averages;
+    }
+
+    /**
+     * Get the average score  for a given student semester.
+     *
+     * @param $studentId  The student.
+     * @param int $semesterId The ID of the semester
+     *
+     * @return void
+     */
+    public function getSemesterAverage($studentId, $semesterId)
+    {
+        return StudentScoreSemester::where('student_id', $studentId)
+            ->where('semester_id', $semesterId)
+            ->value('avage_score');
+    }
+
+
+    /**
+     * Get the average score year  for a given student semester.
+     *
+     * @param int $studentId The ID of the student
+     *
+     * @return void
+     */
+    public function getYearlyAverage($studentId)
+    {
+        return StudentScoreSemester::where('student_id', $studentId)
+            ->where('semester_id', 3)
+            ->value('avage_score');
+    }
+
+    /**
+     * Get Rank  for a given student semester.
+     *
+     * @param int $studentId The ID of the student
+     *
+     * @return void
+     */
+    public function getRank($studentId)
+    {
+        return StudentScoreSemester::where('student_id', $studentId)
+            ->value('rank');
+    }
 
     /**
      * Get My Exam .
      *
-     * @param int $class_id The ID of the class_id
+     * @param Request $request  The HTTP request object.
+     * @param int     $class_id The ID of the class_id
      *
      * @return \Illuminate\View\View
      */
@@ -471,7 +585,8 @@ class ExamService
     /**
      * Get My Exam Teacher.
      *
-     * @param int $user_id The ID of the user
+     * @param Request $request The HTTP request object.
+     * @param int     $user_id The ID of the user
      *
      * @return \Illuminate\View\View
      */
@@ -535,8 +650,7 @@ class ExamService
 
         foreach ($examScores as $studentId => $scores) {
             foreach ($scores as $scoreData) {
-                if (
-                    !empty($studentId)
+                if (!empty($studentId)
                     && !empty($scoreData['exam_id'])
                     && !empty($scoreData['score'])
                 ) {
